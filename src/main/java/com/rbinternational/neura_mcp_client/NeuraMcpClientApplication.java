965c1216a5025aa0968bb38192d3eabfdeac939e
ArdiJorganxhi <ardijorganxhiu@gmail.com>
2025-08-09 11:43:53 +0200

    initial commit

---
+++ src/main/java/com/rbinternational/neura_mcp_client/NeuraMcpClientApplication.java
@@ -0,0 +1,13 @@
+package com.rbinternational.neura_mcp_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuraMcpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuraMcpClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbinternational/neura_mcp_client/NeuraMcpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbinternational.neura_mcp_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuraMcpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
